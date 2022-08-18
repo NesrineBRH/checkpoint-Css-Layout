3cc28f7f5826394370075743da02bb78fdee661d
NesrineBRH <benrhaiemnessrine@gmail.com>
2022-08-18 22:45:41 +0100

    mise à jour18/08/2022

---
+++ function jourDeLasemaine (nombre).js
@@ -0,0 +1,35 @@
+var nombre=0;
+function jourDeLasemaine (nombre)
+{
+    switch (nombre) {
+    case 1:
+      console.log("Dimanche");
+      break;
+      case 2:
+        console.log("Lundi");
+        break;
+    
+        case 3:
+        console.log("Mardi");
+        break;
+        case 4:
+        console.log("Mercredi");
+        break;
+        case 5:
+        console.log("Jeudi");
+        break;
+        case 6:
+            console.log("Vendredi");
+            break;
+
+            case 7:
+                console.log("Samedi");
+                break;
+
+}
+}
+//exercice 2
+let time=0;
+
+    var water=Math.trunc(time*0.5);
+    console.log('the drinken water =($water)L')
